47fb12f1af86137d13b5995c9bec2cc034435044
DorainGeraut <dorian.geraut@telecomnancy.net>
2017-11-29 11:06:04 +0100

    my_python_printer (server) is now multithreaded and can handle multiple clients. For being shuted down all the clients need to be killed

--- scripts/my_python_printer.py
+++ scripts/my_python_printer.py
@@ -1,24 +1,50 @@
-import os
 import sys
 import socket
 import signal
+from threading import Thread
+
+class Connexion(Thread):
+   
+   def __init__(self,conn_sock):
+      print "connection established"
+      self.conn_sock=conn_sock
+      Thread.__init__(self)
 
 
-print "************ Printer ************"
-print "pid: ",os.getpid()
-print "uname(): ",os.uname()
+   def run(self):
+      print "connection running"
+      try:
+         while 1:
+         #Wait for a message from client
+            buff = self.conn_sock.recv(1024)
+            if len(buff) > 0:
+               if buff=="CLOSE_CONNECTION":
+                  break
+               else:
+                  print buff
+   
+      except KeyboardInterrupt:
+         print "connection recieved Keyboard Interrupt!"
+      
+      print "connection ended"
+      self.conn_sock.close()
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', 55000))
-s.listen(5)
-conn, addr = s.accept()
+class Printer(object):
 
-while 1:
-#Wait for a message from client
-   buff = conn.recv(1024)
-   if len(buff) > 0:
-      if buff=="CLOSE_CONNECTION":
-         break
-      else:
-         print buff
-print "deconnection of client"
+   def __init__(self):
+   
+      print "************ Printer ************"
+      self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      self.s.bind(('', 55000))
+      self.s.listen(5)
+      
+      while 1:
+         conn_sock, addr = self.s.accept()
+         try:
+            c = Connexion(conn_sock)
+            c.start()
+         except KeyboardInterrupt:
+            sys.exit()
+if __name__ == '__main__':
+   p = Printer()
