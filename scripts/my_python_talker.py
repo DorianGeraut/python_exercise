27fd3efc893adbba608d0c56e505fcd4d1b4ed15
DorainGeraut <dorian.geraut@telecomnancy.net>
2017-11-29 15:51:47 +0100

    talker now end correctly

--- scripts/my_python_talker.py
+++ scripts/my_python_talker.py
@@ -1,20 +1,20 @@
 import sys
 import socket
 import time
-from threading import Thread
+from threading import Thread, Event
 
 class Talker(Thread):
    
    def __init__(self):
       
       print "************ Talker ************"
+      self.event = Event()
       self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
       self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
       Thread.__init__(self)
 
    def run(self):
-      print "running"
-      while 1:
+      while not self.event.is_set():
          try:
             self.s.connect(('localhost', 55000))
             while 1:
@@ -27,9 +27,18 @@ class Talker(Thread):
                else:
                   self.s.send(str(m))              
          except socket.error:
-            time.sleep(5)
-            print "sleeping"
+            time.sleep(1)
+
+   def stop(self):
+      self.s.close()
+      self.event.set()
 
 if __name__ == '__main__':
    t = Talker()
    t.start()
+   try:
+      while 1:
+         time.sleep(1)
+   except KeyboardInterrupt:
+      t.stop()
+      exit(0)
